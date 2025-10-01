f56394a2ce317240ac93224d258395d11158f648
mykhailokorbiak <mykhailo.korbiak.ap.2024@lpnu.ua>
2025-10-01 09:07:02 +0300

    122133

--- lab6/lab6/opamp.cpp
+++ lab6/lab6/opamp.cpp
@@ -1,16 +1,13 @@
 #include "opamp.h"
 
-// Конструктор
 OpAmpArray::OpAmpArray() {
     std::cout << "Створено масив операційних підсилювачів (" << SIZE << " шт.)\n";
 }
 
-// Деструктор
 OpAmpArray::~OpAmpArray() {
     std::cout << "Знищення масиву операційних підсилювачів\n";
 }
 
-// Перевірка правильності даних
 bool OpAmpArray::validate(const OpAmp &amp) const {
     if (amp.gain <= 0) return false;
     if (amp.voltage <= 0 || amp.voltage > 50) return false;
@@ -18,7 +15,6 @@ bool OpAmpArray::validate(const OpAmp &amp) const {
     return true;
 }
 
-// Введення даних
 void OpAmpArray::inputData() {
     for (int i = 0; i < SIZE; i++) {
         OpAmp amp;
@@ -35,14 +31,13 @@ void OpAmpArray::inputData() {
 
         if (!validate(amp)) {
             std::cout << "❌ Помилка: некоректні дані. Повторіть введення.\n";
-            i--; // повторити для цього елемента
+            i--; 
         } else {
             opamps[i] = amp;
         }
     }
 }
 
-// Виведення даних
 void OpAmpArray::outputData() const {
     std::cout << "\nСписок операційних підсилювачів:\n";
     for (int i = 0; i < SIZE; i++) {
